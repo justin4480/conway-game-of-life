e92decdebb21d29e61152942b1ca657d94f4d240
Justin Davis <justin4480@gmail.com>
2022-11-29 22:36:43 +0000

    formatting

--- __main__.py
+++ __main__.py
@@ -1,15 +1,16 @@
 from itertools import count
+
 import pygame
+
+from src.pattern import get_patterns
+from src.simulator import game_of_life
 from src.view import View
 from src.world import World, upsample
-from src.simulator import game_of_life
-from src.pattern import get_patterns
-
 
 WIDTH = 500
 HEIGHT = 500
 PIXEL_SIZE = 1
-FPS = -1
+FPS = 120
 N_PATTERNS = 500
 
 
@@ -21,8 +22,8 @@ def main():
     font = pygame.font.SysFont("Arial", 12)
     screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 
-    patterns = get_patterns(n_patterns=N_PATTERNS, proba=[.1, .1, .8])
-    world = World(int(HEIGHT/PIXEL_SIZE), int(WIDTH/PIXEL_SIZE))
+    patterns = get_patterns(n_patterns=N_PATTERNS, proba=[0.1, 0.1, 0.8])
+    world = World(int(HEIGHT / PIXEL_SIZE), int(WIDTH / PIXEL_SIZE))
     world.populate_world(patterns)
     simulator = game_of_life(world.array)
     view = View()
@@ -33,11 +34,10 @@ def main():
     while not terminate:
         terminate = view.process_events()
         frame = upsample(next(simulator), PIXEL_SIZE).T * 255
-        fps = font.render(
-            f'frame {next(i)} | fps: {str(int(clock.get_fps()))}',
-            1, pygame.Color("coral")
+        fps_text = font.render(
+            f"frame {next(i)} | fps: {str(int(clock.get_fps()))}", 1, pygame.Color("coral")
         )
-        view.display_frame(screen, frame, fps)
+        view.display_frame(screen, frame, fps_text)
         clock.tick(FPS)
 
     pygame.quit()
