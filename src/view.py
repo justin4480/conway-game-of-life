e7a6822d64e518237b8dcf767d8927710cae6f22
Justin Davis <justin4480@gmail.com>
2022-12-14 00:12:02 +0000

    img save

--- src/view.py
+++ src/view.py
@@ -9,9 +9,10 @@ class View:
                 return True
         return False
 
-    def display_frame(self, screen, frame: np.array, fps_text=None) -> None:
+    def display_frame(self, screen, frame: np.array, fps_text=None, i=0) -> None:
         surfarray = pygame.surfarray.make_surface(frame)
         screen.blit(surfarray, (0, 0))
         if fps_text:
             screen.blit(fps_text, (10, 10))
         pygame.display.update()
+        pygame.image.save(surfarray, f"img/image{i}.jpg")
